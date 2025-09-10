17831ef969a327ea7f258847447685c2a0af5916
SyedBilal61 <bilalkazmi61.bk@gmail.com>
2025-09-10 23:38:36 +0200

    Add failing test App2Test

---
+++ src/test/java/App2Test.java
@@ -0,0 +1,19 @@
+
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class App2Test {
+    private App app;
+
+    @Before
+    public void setup() {
+        app = new App();
+    }
+
+    @Test
+    public void testSayHello() {
+        assertEquals("Hallo", app.sayHello());
+    }
+}
